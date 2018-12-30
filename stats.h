ce0fd8b2912d80c831a40f168dd02dd2c7c59bc1
A. Faruk UNAL <unalfaruk@outlook.com>
2018-12-30 15:11:10 +0300

    Function prototypes and descriptions were defined.

--- stats.h
+++ stats.h
@@ -21,20 +21,108 @@
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+
+ /**
+ * @brief <To return the all elements of the array.>
+ *
+ * <This function prints all elements of the array.>
+ *
+ * @param <ptrOfArray> <The reference for the array>
+ * @param <lengthOfArray> <The length for the array>
+ *
+ * @return <All elements of the araay are returned.>
+ */
+void print_array(unsigned char * ptrOfArray, unsigned int lengthOfArray);
+
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief <To return the maximum value of the array.>
  *
- * <Add Extended Description Here>
+ * <This function scans all elements of the array, compares them each other and returns the maximum value.>
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param <ptrOfArray> <The reference for the array>
+ * @param <lengthOfArray> <The length for the array>
  *
- * @return <Add Return Informaiton here>
+ * @return <The maximum value are returned as an unsigned integer.>
  */
+void find_maximum(unsigned char * ptrOfArray, unsigned int lengthOfArray);
 
+
+/**
+* @brief <To return the minimum value of the array.>
+*
+* <This function scans all elements of the array, compares them each other and returns the minimum value.>
+*
+* @param <ptrOfArray> <The reference for the array>
+* @param <lengthOfArray> <The length for the array>
+*
+* @return <The minimum value are returned as an unsigned integer.>
+*/
+void find_minimum(unsigned char * ptrOfArray, unsigned int lengthOfArray);
+
+
+/**
+* @brief <To return the mean value of the array.>
+*
+* <This function scans all elements of the array, sums all of them and divide the total value by element numbers, then returns the mean value.>
+*
+* @param <ptrOfArray> <The reference for the array>
+* @param <lengthOfArray> <The length for the array>
+*
+* @return <The mean value are returned as an unsigned integer.>
+*/
+void find_mean(unsigned char * ptrOfArray, unsigned int lengthOfArray);
+
+
+/**
+* @brief <To sort the elements of the array.>
+*
+* <This function sorts all elements of the array from largest to smallest.>
+*
+* @param <ptrOfArray> <The reference for the array>
+* @param <lengthOfArray> <The length for the array>
+*
+* @return <All elements are returned in order.>
+*/
+void sort_array(unsigned char * ptrOfArray, unsigned int lengthOfArray);
+
+
+/**
+* @brief <To return the median value of the array.>
+*
+* <This function sorts the array and finds the middle element.>
+*
+* @param <ptrOfArray> <The reference for the array>
+* @param <lengthOfArray> <The length for the array>
+*
+* @return <The median value are returned as an unsigned integer.>
+*/
+void find_mean(unsigned char * ptrOfArray, unsigned int lengthOfArray);
+
+
+/**
+* @brief <To return the median value of the array.>
+*
+* <This function sorts the array and finds the middle element.>
+*
+* @param <ptrOfArray> <The reference for the array>
+* @param <lengthOfArray> <The length for the array>
+*
+* @return <The median value are returned as an unsigned integer.>
+*/
+void find_median(unsigned char * ptrOfArray, unsigned int lengthOfArray);
+
+
+/**
+* @brief <To return the all information about the array like median, mean, maximum and minimum value.>
+*
+* <This function runs all other functions to show information about the array.>
+*
+* @param <ptrOfArray> <The reference for the array>
+* @param <lengthOfArray> <The length for the array>
+*
+* @return <The outputs of all other functions returned.>
+*/
+void print_statistics(unsigned char * ptrOfArray, unsigned int lengthOfArray);
 
 #endif /* __STATS_H__ */
